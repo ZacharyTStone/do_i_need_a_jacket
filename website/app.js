7ef218a0c5f45eb7611458901e203f43461c391d
Zach Stone <zachinjapan888s@gmail.com>
2021-09-26 19:37:51 -0400

    initial commit

---
+++ website/app.js
@@ -0,0 +1,20 @@
+/* Global Variables */
+// API URL/Key
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const APIKey = "&appid=f0e2f2fd5311df4ea9bdff1d071ad35e";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// helper functions
+
+// Get weather info from API
+
+async function getWeatherData(baseURL, zipcode, APIKey) {
+
+}
+// test
+
+
+// main functions
