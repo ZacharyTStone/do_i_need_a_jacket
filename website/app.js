51be72c685aac01ba3547fdda5c4b8605c32a690
Zach Stone <zachinjapan888s@gmail.com>
2021-09-26 19:56:47 -0400

    app.js work

--- website/app.js
+++ website/app.js
@@ -13,12 +13,31 @@ let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 async function getWeatherData(baseURL, zipcode, APIKey) {
     // makes a fetch request to the api url and saves the response (await means it doesn't move on till it is complete)
-    const response = await fetch(baseURL + city + key);
+    const response = await fetch(baseURL + zipcode + APIKey);
     // save that response as a json file
     const data = await response.json();
+    // then tries to return that data to be used in the web app and catches if it doesnt work
+    try {
+        console.log("returned the data from the data JSON");
 
-}
+        return {
+            // uses the json file and finds some data from that using dot notation
+            city: data.name,
+            country: data.sys.country,
+            temp: data.main.temp,
+            description: data.weather[0].description,
+        };
+
+    } catch (error) {
+        console.log("Error. Could not return data JSON file", error);
+    };
+
+};
+
+
 // test
 
 
 // main functions
+
+getWeatherData(baseURL, 19128, APIKey);
